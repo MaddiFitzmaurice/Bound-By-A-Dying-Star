69c2b9f13d591d1917f739ce6a752853d5ec7436
Maddi <maddi.fitzmaurice@gmail.com>
2024-04-08 19:55:49 +1000

    Hierarchy Cleanup
    - Cleaned up the hierarchy to make it easily readable

---
+++ Assets/Scripts/Camera/CameraTrigger.cs
@@ -0,0 +1,16 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrigger : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera _cam;
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
