4072d5cc0363518b7d94f7550a291eae51784fdd
R0binicus <102570571+R0binicus@users.noreply.github.com>
2024-05-08 23:10:26 +1000

    CAPS-191 Level 1 - Sky Constellation D
    - Made temp art for sky constellation beams
    - Made VFX shader graphs + materials for sky constellation beams
    - Affordance beam can now be used with any beam prefab
    - Affordance beam now named Beam Emitter
    - Put sky constellation beams onto Level 1 sky constellation

---
+++ Assets/Scripts/Level1/BeamEmitter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeamEmitter : MonoBehaviour
+{
+    [SerializeField] private LineRenderer _beamRenderer;
+    [SerializeField] private Transform _beamSource;
+    [SerializeField] private Transform _beamDestination;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set start and end point of the beam in local space
+        _beamRenderer.SetPosition(0, transform.InverseTransformPoint(_beamSource.position));
+        _beamRenderer.SetPosition(1, transform.InverseTransformPoint(_beamDestination.position));
+        _beamRenderer.enabled = true;
+    }
+}
