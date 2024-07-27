2b6989e64267cfdface46b741e96a9bc28a2b5ac
Maddi <maddi.fitzmaurice@gmail.com>
2024-07-27 19:56:40 +1000

    Key Bob 2

--- Assets/Scripts/Level1/KeyBob.cs
+++ Assets/Scripts/Level1/KeyBob.cs
@@ -6,13 +6,12 @@ public class KeyBob : MonoBehaviour
 {
     [SerializeField] private float _bobTime;
     [SerializeField] private float _bobDisplacement = 1f;
-    [SerializeField] private LeanTweenType _easeInType;
-    [SerializeField] private LeanTweenType _easeOutType;
+    [SerializeField] private LeanTweenType _easeType;
 
     // Update is called once per frame
     void Start()
     {
         Vector3 moveTo = new Vector3(this.transform.position.x, this.transform.position.y + _bobDisplacement, this.transform.position.z);
-        LeanTween.move(this.gameObject, moveTo, _bobTime).setEase(_easeInType).setLoopPingPong().setEase(_easeOutType);
+        LeanTween.move(this.gameObject, moveTo, _bobTime).setEase(_easeType).setLoopPingPong();
     }
 }
