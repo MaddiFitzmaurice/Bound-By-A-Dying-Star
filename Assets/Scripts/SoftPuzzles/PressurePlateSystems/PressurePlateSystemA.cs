c21614a658ba91c03765b05660ff04823593315b
Maddi <maddi.fitzmaurice@gmail.com>
2024-09-27 14:35:52 +1000

    CAPS-518 Ghost Statues
    - Added ghost effect to statues so players know where the object should appear again when a pressure plate is activated/deactivated

--- Assets/Scripts/SoftPuzzles/PressurePlateSystems/PressurePlateSystemA.cs
+++ Assets/Scripts/SoftPuzzles/PressurePlateSystems/PressurePlateSystemA.cs
@@ -6,13 +6,37 @@ using UnityEngine;
 // Pressure Plate System A: Lock/Show and Unlock/Hide an Object
 public class PressurePlateSystemA : PressurePlateSystem
 {
-    // External Data
+    #region EXTERNAL DATA
     [SerializeField] private GameObject _affectedObj; // Object to lock/unlock
+    [SerializeField] private Material _ghostEffectMat;
     [SerializeField] private bool _onIsUnlock = true; // Does Object unlock or lock when pressure plate is stepped on?
-   
+    #endregion
+
+    #region INTERNAL DATA
+    private List<Renderer> _affectedObjRenderers;
+    private Collider _affectedObjCollider;
+    private List<Material> _normalMats;
+    #endregion
     private void Awake()
     {
+        // Init pressure plates
         InitAllPressurePlates();
+
+        // Init components
+        _affectedObjCollider = _affectedObj.GetComponent<Collider>();
+        _affectedObjRenderers = _affectedObj.GetComponentsInChildren<Renderer>().ToList();
+
+        // Record default materials of each affected renderer
+        _normalMats = new List<Material>();
+        for (int i = 0;  i < _affectedObjRenderers.Count; i++)
+        {
+            _normalMats.Add(_affectedObjRenderers[i].material);
+        }
+
+        if (_normalMats.Count != _affectedObjRenderers.Count)
+        {
+            Debug.LogError("Num of materials does not match number of renderers!");
+        }
     }
 
     public override void PlateActivated(PressurePlateSingle plate, bool activated)
@@ -42,6 +66,20 @@ public class PressurePlateSystemA : PressurePlateSystem
         // Decide what the object should do when a plate is triggered
         bool active = _onIsUnlock ? !activated : activated;
 
-        _affectedObj.SetActive(active);
+        if (active)
+        {
+            for (int i = 0; i < _affectedObjRenderers.Count; i++)
+            {
+                _affectedObjRenderers[i].material = _normalMats[i];
+            }
+        }
+        else
+        {
+            for (int i = 0; i < _affectedObjRenderers.Count; i++)
+            {
+                _affectedObjRenderers[i].material = _ghostEffectMat;
+            }
+        }
+        _affectedObjCollider.enabled = active;
     }
 }
